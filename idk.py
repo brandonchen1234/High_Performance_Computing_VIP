f643de4d08a24d3cff87f993a9c64d14d8c3baf4
brand <brandonchen368@gmail.com>
2026-04-20 11:39:34 -0400

    Initial commit

---
+++ idk.py
@@ -0,0 +1,9 @@
+import qiskit
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit import Aer, execute
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+
